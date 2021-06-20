dc82c28540803f5102cca08715d6f18d4d7cfab3
cevatarmutlu <cevatarmutlu@gmail.com>
2021-06-20 17:06:27 +0300

    remove req1 folder

--- code/req1/main.py
+++
@@ -1,6 +0,0 @@
-import src.a as a, src.b as b, src.c as c
-
-if __name__ == "__main__":
-    a.main()
-    b.main()
-    c.main()
--- code/req1/src/conn.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-import src.config as config
-
-
-def get_conn():
-    postgres_cfg = config.get('postgres')
-    conn = psycopg2.connect(**postgres_cfg)
-    return conn
